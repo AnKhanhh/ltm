b003bef5a4327495165c3ea11bd49ef142dae84c
AnKhanhh <93918878+AnKhanhh@users.noreply.github.com>
2023-04-17 22:38:32 +0700

    input success

--- 1104/tcp_client.c
+++ 1104/tcp_client.c
@@ -13,27 +13,30 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 
-	char *a, *ip = argv[1];
+	char *ip = argv[1];
+	char *a;
 	int port = (int) strtol(argv[2], &a, 10); // 80 = http, 443 = https
-	free(a);
 
-	int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	struct sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_addr.s_addr = inet_addr(ip);
-	addr.sin_port = htons(port);
-
-	int ret = connect(client, (struct sockaddr *) &addr, sizeof(addr));
-	if (ret == -1) {
-		perror("connect() unsuccessfully");
-		return 1;
-	}
-
-	char msg[256];
-	fgets(msg, sizeof(msg), stdin);
-	send(client, msg, strlen(msg), 0);
-
-	free(ip);
+//
+//	int client = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+//	struct sockaddr_in addr;
+//	addr.sin_family = AF_INET;
+//	addr.sin_addr.s_addr = inet_addr(ip);
+//	addr.sin_port = htons(port);
+//
+//	int ret = connect(client, (struct sockaddr *) &addr, sizeof(addr));
+//	if (ret == -1) {
+//		perror("connect() unsuccessfully");
+//		return 1;
+//	}
+//
+//	char msg[256] = "hello";
+//	fflush(stdin);
+//	fgets(msg, sizeof(msg), stdin);
+//	send(client, msg, strlen(msg), 0);
+//
+//	free(ip);
 //	close(client);
+//	free(a);
 	return 0;
 }
